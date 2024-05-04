e6e314f376abf4c3411c91706bb95a01bc2e1c60
csabibme <36829084+csabibme@users.noreply.github.com>
2024-05-04 22:47:37 +0200

    FINAL

--- src/main/java/org/example/Csiga.java
+++ src/main/java/org/example/Csiga.java
@@ -1,12 +1,14 @@
 package org.example;
-
+import java.util.Random;
 public class Csiga implements Versenyzo {
+    private Random rnd;
     private String szin;
     private int tavolsag;
 
     public Csiga(String szin) {
         this.szin = szin;
         this.tavolsag = 0;
+        this.rnd=new Random();
     }
 
     @Override
@@ -21,7 +23,9 @@ public class Csiga implements Versenyzo {
 
     @Override
     public void lep() {
-        tavolsag += 1;  // normál sebesség
+        int lepesek = rnd.nextInt(4); // A lépések a videon elhnagzottak szerint 0-3 közöttiek lehetnek
+        tavolsag += lepesek;
+        System.out.println(szin + " csiga " + lepesek + " lépést tett.");
     }
 
     @Override
