8cb7a351ee3c57c1b3afc2740d0f3b137e9bb029
csabibme <36829084+csabibme@users.noreply.github.com>
2024-05-04 21:26:51 +0200

    csak megfelelo szineke fogad el

--- src/main/java/org/example/Versenykiiras.java
+++ src/main/java/org/example/Versenykiiras.java
@@ -6,22 +6,19 @@ import java.util.Scanner;
 public class Versenykiiras {
     private Versenyzo[] versenyzok;
     private Random szam = new Random();
+    private Scanner scanner = new Scanner(System.in);
 
     public Versenykiiras(Versenyzo[] versenyzok) {
         this.versenyzok = versenyzok;
     }
 
     public void versenyIndit() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Tippelj, melyik csiga lesz a győztes! (piros, zöld, kék):");
-        String tipp = scanner.nextLine(); // ide majd be kell tennem inputra exceptiont...
+        String tipp = tippEllenorzes();
 
         for (int kor = 1; kor <= 5; kor++) {
             System.out.println("Kör: " + kor);
             for (int i = 0; i < versenyzok.length; i++) {
                 if (szam.nextInt(5) == 0) {
-                    /*20% beállítása.
-                    (ahhoz hogy 0 legyen a random szam, 1 az 5höz az esély, vagyis épp 20%)*/
                     versenyzok[i].gyorsit();
                     System.out.println(versenyzok[i].getSzin() + " gyorsítót kaptott...");
                 } else {
@@ -36,17 +33,37 @@ public class Versenykiiras {
 
         Versenyzo gyoztes = gyoztesKivalaszt();
         System.out.println("A nyertes: " + gyoztes.getSzin() + "!");
-        if (gyoztes.getSzin().equals(tipp)) {
+        if (gyoztes.getSzin().equalsIgnoreCase(tipp)) {
             System.out.println("A tipped helyes volt");
         } else {
             System.out.println("Nem tippeltél helyesen.");
+            // az a gond, hogy a döntetlennél matematikailag bármely tipp helyes. Ezt kezelni kell!
         }
         scanner.close();
     }
 
+    private String tippEllenorzes() { //metódus az ellenőrzéshez
+        System.out.println("Tippelj, melyik csiga lesz a győztes! (piros, zöld, kék):");
+        String tipp = scanner.nextLine().trim();
+        while (!ervenyesSzin(tipp)) {
+            System.out.println("Csak a következők adhatók meg: (piros, zöld, kék):");
+            tipp = scanner.nextLine().trim();
+        }
+        return tipp;
+    }
+
+    private boolean ervenyesSzin(String szin) {
+        for (Versenyzo versenyzo : versenyzok) {
+            if (versenyzo.getSzin().equalsIgnoreCase(szin)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private Versenyzo gyoztesKivalaszt() {
         Versenyzo gyoztes = versenyzok[0];
-        for (int i = 0; i < versenyzok.length; i++) {
+        for (int i = 1; i < versenyzok.length; i++) {
             if (versenyzok[i].getTavolsag() > gyoztes.getTavolsag()) {
                 gyoztes = versenyzok[i];
             }
@@ -54,4 +71,3 @@ public class Versenykiiras {
         return gyoztes;
     }
 }
-
